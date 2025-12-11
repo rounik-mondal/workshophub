7624fd4b3e7b05bd18faef3316eda356641eb840
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-12 00:31:47 +0530

    feat(auth): implement login and logout

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -23,3 +23,26 @@ const signup = async (req, res) => {
         res.status(500).json({message: "Server error"});
     }
 };
+
+const login = async (req, res) => {
+    try {
+        const {email, password} = req.body;
+        if (!email || !password) return res.status(400).json({message: "Email and password are both required"});
+        const user = await User.findOne({email});
+        if (!user) return res.status(400).json({message: "Invalid credentials"});
+
+        const passwordOkay = await bcrypt.compare(password, user.password);
+        if (!passwordOkay) return res.status(400).json({message: "Invalid credentials"});
+
+        const token = jwt.sign({id: user._id}, process.env.JWT_SECRET, {expiresIn: process.env.JWT_EXPIRES_IN || '7d'});
+        res.json({message: "Login successful", token, user: {id: user._id, name: user.name, email: user.email, role: user.role}});
+    }
+    catch (err) {
+        console.error(err);
+        res.status(500).json({message: "Server error"});
+    }
+};
+
+const logout = async (req, res) => {
+    res.json({message: "Logout successful. Client should remove token from storage."})
+}
