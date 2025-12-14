1641b313da88ca5d3bd74c8bc7a3f4d4de9734e3
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-15 00:54:12 +0530

    feat(controllers): add incomplete registration controller code

---
+++ backend/controllers/registrationController.js
@@ -0,0 +1,7 @@
+const Registration = require('../models/Registration');
+
+// will implement in the morning
+
+const registerForWorkshop = async (req, res) => {};
+
+const listRegistrations = async (req, res) => {};
