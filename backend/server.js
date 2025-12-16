2df3e55af621e754659422f4adb4d68a5af7c35d
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-17 00:59:10 +0530

    refactor(server): slightly organise server.js

--- backend/server.js
+++ backend/server.js
@@ -4,10 +4,9 @@ const cors = require('cors');
 const connectDB = require('./config/db');
 
 dotenv.config();
+connectDB();
 
 const app = express();
-
-connectDB();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
