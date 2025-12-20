69bf93cf2ad7bfd92d276ad4a1ef858509379783
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-20 17:20:07 +0530

    refactor(attendance): remove TODO comment as it is done now

--- backend/routes/attendanceRoutes.js
+++ backend/routes/attendanceRoutes.js
@@ -5,6 +5,5 @@ const {verifyToken, requireRole} = require('../middleware/authMiddleware');
 
 router.post('/mark', verifyToken, requireRole(['instructor', 'admin']), markAttendance);
 router.get('/workshop/:workshopId', verifyToken, requireRole(['instructor', 'admin']), getAttendanceByWorkshop);
-// TODO in a future version: restrict instructors to getting attendance details for their own workshops only
 
 module.exports = router;
