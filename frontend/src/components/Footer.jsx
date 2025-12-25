bdf4b5dd40950cf2050940c489709de0d6e7bafc
Saptaparno Chakraborty <schakraborty1086@gmail.com>
2025-12-26 00:07:50 +0530

    feat: add footer component

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,13 @@
+export default function Footer() {
+    return (
+        <footer className='bg-linear-to-r from-gray-900 via-blue-900 to-gray-800 text-center p-6 mt-8 shadow-inner rounded-t-2xl'>
+            <p className='text-gray-200 font-bold text-lg mb-1'>
+                Built by <span className='text-yellow-400'>Saptaparno Chakraborty</span>
+            </p>
+            <p className='text-gray-400 text-sm tracking-wide'>
+                First MERN Learning Project • 2025
+            </p>
+            <div className='mt-3 h-1 w-24 mx-auto bg-yellow-400 rounded-full shadow-md'></div>
+        </footer>
+    );
+}
